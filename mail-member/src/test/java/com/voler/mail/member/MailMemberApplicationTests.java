964e8a95183b19eff4810adedd7d04d9949a2849
wyf <56500839@qq.com>
2022-12-10 15:02:42 +0800

    项目初始结构创建

---
+++ mail-member/src/test/java/com/voler/mail/member/MailMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voler.mail.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
